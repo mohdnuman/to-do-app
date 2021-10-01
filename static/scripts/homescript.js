aa16480d31d657c7708af27216dfc5ae60df4a8e
Mohd Numan <mohammadnuman123@gmail.com>
2021-10-01 17:03:30 +0530

    added strikethorugh effect

---
+++ static/scripts/homescript.js
@@ -0,0 +1,10 @@
+console.log("script is running");
+myfunction(){
+    var checkbox=document.getElementById("check");
+
+    var text=checkbox.getAttribute("value");
+
+    if(checkbox.checked==true){
+        console.log(checked);
+    }
+}
